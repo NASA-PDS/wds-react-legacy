b1058fd135bb14118c6b74159e73ee876c97d9fc
Eddie Arevalo <eddiesarevalo@gmail.com>
2022-02-22 16:55:25 -0800

    Fix Hover Over Status Crash Bug #27
    -Fixed a bug where hovering over the status value in a client app would cause a crash.
    -React has been updated to the latest version which fixes this issue.
    -Storybook has been dropped as it does not work on React versions 17 and up.

--- src/pds/DoiSearch/DoiSearch.stories.js
+++
@@ -1,9 +0,0 @@
-// /src/components/Button/button.stories.js
-import React from 'react'
-import DoiSearch from './DoiSearch'
-import { withKnobs, text } from '@storybook/addon-knobs'
-
-export default { title: 'DOI Search', decorators: [withKnobs]}
-export const doiSearch = () => {
-  return <DoiSearch/>
-}
