3643da88954f57c3ed488ddf2c9be14fd4377040
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-07-20 10:48:37 -0700

    Removed test application to clean up the project so that components can be moved to the src folder to allow for the integration of storybook which was added in this commit with boilerplate component/story examples.

--- src/components/TestComponent/index.test.tsx
+++ src/components/TestComponent/index.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import renderer from 'react-test-renderer'
 import { describe, expect, test } from 'vitest'
 import TestComponent from './index'
 
 describe('TestComponent', () => {
+
   test('TestComponent component renders correctly', () => {
     const component = renderer.create(
       <TestComponent />
@@ -23,4 +23,5 @@ describe('TestComponent', () => {
 
     expect(tree).toMatchSnapshot()
   })
+  
 })
