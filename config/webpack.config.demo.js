3b88c9b3dcecc7c5ff0c418f18bcd6a94c349e20
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-10-03 13:19:36 -0700

    Removed usages of `WatchMissingNodeModulesPlugin` because it is not available for Webpack versions >=5.

--- config/webpack.config.demo.js
+++ config/webpack.config.demo.js
@@ -16,7 +16,6 @@ const safePostCssParser = require('postcss-safe-parser');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 const paths = require('./paths');
